d66146751f05afc0682d6e8834a7c8f6ad22e1a6
quocdaijr <quocdaijr@gmail.com>
2021-10-14 09:58:46 +0700

    update highlight code

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,6 +1,6 @@
 import Prism from 'prismjs'
-import 'prismjs/themes/prism-okaidia.css' // You can add other themes if you want
-import '@/assets/sass/prism.sass'
+import 'prismjs/themes/prism-tomorrow.css' // You can add other themes if you want
+import '@/assets/css/prism.css'
 
 // Include the toolbar plugin: (optional)
 import "prismjs/plugins/toolbar/prism-toolbar";
@@ -19,9 +19,33 @@ import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 import "prismjs/plugins/highlight-keywords/prism-highlight-keywords";
 import "prismjs/plugins/show-language/prism-show-language";
 
+import "prismjs/plugins/previewers/prism-previewers";
+import "prismjs/plugins/previewers/prism-previewers.css";
+
 // Include additional languages
-import "prismjs/components/prism-bash.js";
-// Set vue SFC to markdown
-Prism.languages.vue = Prism.languages.markup;
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-markup-templating";
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-sass';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-php-extras';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-django';
+import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-nginx';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-git';
+import 'prismjs/components/prism-regex';
+import 'prismjs/components/prism-bash';
 
 export default Prism
