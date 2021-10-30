41946216bfe205028fbf905045f7881e2c7df353
quocdaijr <quocdaijr@gmail.com>
2021-10-30 10:13:57 +0700

    fix height

--- components/HeaderContent.vue
+++ components/HeaderContent.vue
@@ -1,9 +1,8 @@
 <template>
   <header class=" flex-col px-6 py-12 border-b border-gray-200 lg:px-8 xl:pt-16 dark:border-gray-600">
-
     <div v-if="background" class="max-w-screen-lg mx-auto font-semibold text-center flex-grow">
-      <img :src="$urlResize(background, '480p') || 'http://st.basic.laravel.local/i/480p/2021/10/12/logo-1634049246.png'"
-           :title="title" class="w-full h-full" :alt="title">
+      <img :src="$urlResize(background, '720p') || ''"
+           :title="title" class="w-full h-48 shadow-md" :alt="title">
     </div>
     <div class="max-w-screen-lg mx-auto font-semibold text-center mt-4">
       <h1 v-if="title" class="text-3xl font-bold text-gray-800 lg:text-5xl dark:text-gray-100">{{ title }}</h1>
